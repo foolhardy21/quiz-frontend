b48b28baaf1a633a168c3e041b183fb5c34ba525
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-11 15:25:34 +0530

    style - stylesheet and html added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <header id="pg-header" class="flx flx-maj-start pd-md">
+        <p class="txt-primary txt-lg txt-ucase">
+          sneakerhead
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+
+      <span id="btn-theme" class="material-icons pos-absolute tr-1 icon-primary pd-s">
+        bedtime
+      </span>
+
+      <div class="flx flx-center">
+        <section class="card-dim card-shadow-xs pd-md mg-top-md">
+          <div class="flx flx-maj-stretch mg-btm-md">
+            <p class="txt-cap txt-primary txt-md">q1</p>
+            <p class="txt-cap txt-primary txt-md">score - 1/5</p>
+          </div>
+          <p class="txt-md txt-primary mg-btm-md">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti,
+            consequatur.</p>
+          <div class="flx flx-column">
+            <button
+              class="btn-outlined b-solid b-secondary txt-md txt-primary bg-primary mg-btm-xs pd-xs">lorem</button>
+            <button
+              class="btn-outlined b-solid b-secondary txt-md txt-primary bg-primary mg-btm-xs pd-xs">lorem</button>
+            <button
+              class="btn-outlined b-solid b-secondary txt-md txt-primary bg-primary mg-btm-xs pd-xs">lorem</button>
+            <button
+              class="btn-outlined b-solid b-secondary txt-md txt-primary bg-primary mg-btm-xs pd-xs">lorem</button>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
 
