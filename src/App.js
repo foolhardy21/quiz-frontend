707c89bb1b133b9aba0702a47c2add510f3a5e75
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-11 16:35:26 +0530

    refactor - import statement update

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
+import { Question } from "./components/Question";
 
 const App = () => {
 
   return (
-    <>
-      <header id="pg-header" class="flx flx-maj-start pd-md">
-        <p class="txt-primary txt-lg txt-ucase">
-          sneakerhead
-        </p>
-      </header>
 
-      <span id="btn-theme" class="material-icons pos-absolute tr-1 icon-primary pd-s">
-        bedtime
-      </span>
+    <Question />
 
-      <div class="flx flx-center">
-        <section class="card-dim card-shadow-xs pd-md mg-top-md">
-          <div class="flx flx-maj-stretch mg-btm-md">
-            <p class="txt-cap txt-primary txt-md">q1</p>
-            <p class="txt-cap txt-primary txt-md">score - 1/5</p>
-          </div>
-          <p class="txt-md txt-primary mg-btm-md">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti,
-            consequatur.</p>
-          <div class="flx flx-column">
-            <button
-              class="btn-outlined b-solid b-secondary txt-md txt-primary bg-primary mg-btm-xs pd-xs">lorem</button>
-            <button
-              class="btn-outlined b-solid b-secondary txt-md txt-primary bg-primary mg-btm-xs pd-xs">lorem</button>
-            <button
-              class="btn-outlined b-solid b-secondary txt-md txt-primary bg-primary mg-btm-xs pd-xs">lorem</button>
-            <button
-              class="btn-outlined b-solid b-secondary txt-md txt-primary bg-primary mg-btm-xs pd-xs">lorem</button>
-          </div>
-        </section>
-      </div>
-    </>
   );
 }
 
