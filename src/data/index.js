fbf1d37711558a78da0ddcee33259a217adb2164
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-03-11 22:05:14 +0530

    feat - rules data file

---
+++ src/data/index.js
@@ -0,0 +1,5 @@
+import { rules } from "./rules.data";
+
+export {
+    rules
+}
---
+++ src/data/rules.data.js
@@ -0,0 +1,18 @@
+export const rules = [
+    {
+        id: 1,
+        rule: `There are total 10 questions.`
+    },
+    {
+        id: 2,
+        rule: `There are no negative points.`
+    },
+    {
+        id: 3,
+        rule: `One correct answer gets you 1 point.`
+    },
+    {
+        id: 4,
+        rule: `Good luck.`
+    },
+]
