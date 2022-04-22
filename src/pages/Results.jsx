e1010478d3c52cf82e83933fa596dcc03c34861a
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-04-22 19:49:19 +0530

    feat - navigation added in result page

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,13 +1,21 @@
 import { Header } from "components/Reusable"
 import { useScore, useTheme } from "contexts"
 import { useQuestions } from "contexts/questions.context"
+import { useNavigate } from "react-router-dom"
 import { getBgColor } from 'utils'
 import styles from './results.module.css'
 
 const Results = () => {
-    const { score } = useScore()
-    const { questions } = useQuestions()
+    const { score, setScore } = useScore()
+    const { questions, setQuestions } = useQuestions()
     const { theme } = useTheme()
+    const navigate = useNavigate()
+
+    const handleFinish = () => {
+        setQuestions([])
+        setScore(0)
+        navigate('/')
+    }
 
     return (
         <div
@@ -19,12 +27,15 @@ const Results = () => {
             <Header />
 
             <div className="flx flx-column flx-center">
-                <article className="card-dim card-shadow-xs flx flx-column flx-center mg-top-xlg mg-btm-xlg pd-s">
+                <article className="card-dim card-shadow-xs flx flx-column mg-top-xlg mg-btm-xlg pd-s">
                     <p className="txt-md txt-primary txt-cap">congrats!</p>
                     <p className="txt-md txt-primary txt-cap">you have scored {score} points</p>
+                    <div className="flx flx-maj-end mg-top-xs">
+                        <button onClick={handleFinish} className="btn-solid bg-secondary txt-secondary txt-md txt-cap pd-xs">finish</button>
+                    </div>
                 </article>
                 <p className='txt-lg txt-primary txt-600 txt-cap mg-btm-s'>correct answers</p>
-                <section className={`grid grid-maxcols-3 ${styles.resultGrid} pd-top-s`}>
+                <section className={`grid grid-maxcols-3 ${styles.resultGrid} pd-top-s mg-btm-md`}>
                     {
                         questions.map(question => <article key={question.id} className="card-dim card-shadow-xs flx flx-column flx-center pd-s">
                             <p className="card-txtw-s txt-md txt-primary txt-cap mg-btm-xs">{question.question}</p>
@@ -38,7 +49,6 @@ const Results = () => {
                         </article>)
                     }
                 </section>
-
             </div>
         </div>
     )
