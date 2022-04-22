5e9393110078b38d0a341521a84b6a9160610371
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-04-22 18:27:30 +0530

    feat - context for score

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,1 +1,2 @@
 export { useTheme, ThemeProvider } from './theme.context'
+export { useScore, ScoreProvider } from './score.context'
---
+++ src/contexts/score.context.js
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from "react";
+
+const ScoreContext = createContext()
+
+export const ScoreProvider = ({ children }) => {
+    const [score, setScore] = useState(0)
+
+    return (
+        <ScoreContext.Provider
+            value={{
+                score,
+                setScore,
+            }}
+        >
+            {children}
+        </ScoreContext.Provider>
+    )
+}
+
+export const useScore = () => useContext(ScoreContext)
