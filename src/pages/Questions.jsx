5aba03d2077b4c3cdebfa0c471bf5604b62e2cc5
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-05-12 14:42:16 +0530

    fix - ques image url

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -22,7 +22,7 @@ const Questions = () => {
             const questionsQuery = query(collection(db, 'questions'), where('category', '==', category))
             const questionsDocs = await getDocs(questionsQuery)
             setQuestions(questionsDocs.docs.map((doc, index) => (
-                { counter: index + 1, question: doc._document.data.value.mapValue.fields.question.stringValue, img: doc._document.data.value.mapValue.fields.img.stringValue, answer: doc._document.data.value.mapValue.fields.answer.stringValue, options: doc._document.data.value.mapValue.fields.options.arrayValue.values.map(option => option.stringValue) }
+                { counter: index + 1, question: doc._document.data.value.mapValue.fields.question.stringValue, img: doc._document.data.value.mapValue.fields.img.stringValue ?? '', answer: doc._document.data.value.mapValue.fields.answer.stringValue, options: doc._document.data.value.mapValue.fields.options.arrayValue.values.map(option => option.stringValue) }
             )))
         })()
     }, [])
